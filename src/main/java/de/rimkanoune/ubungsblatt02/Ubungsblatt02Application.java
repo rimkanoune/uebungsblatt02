ac0b05a2535dcdf017e6e414118c7c9ceaa00b6d
rimkanoune <rimkanoune011@gmail.com>
2025-06-22 23:39:13 +0200

    third commit

---
+++ src/main/java/de/rimkanoune/ubungsblatt02/Ubungsblatt02Application.java
@@ -0,0 +1,13 @@
+package de.rimkanoune.ubungsblatt02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ubungsblatt02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ubungsblatt02Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/rimkanoune/ubungsblatt02/Ubungsblatt02ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rimkanoune.ubungsblatt02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ubungsblatt02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
